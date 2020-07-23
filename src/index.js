6d9faa8894e3c327647ab1b780f6674d321df0f8
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-07-23 00:04:19 -0400

    nothing accomplished yet

--- src/index.js
+++ src/index.js
@@ -3,4 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+function App()  {
+
+
+
+    return (
+
+
+    )
+}
+
+
+// const [value, setValue] = useState();
+
+// function App() {
+
+//     const [count, setCount] = useState(0);
+//     const [title, setTitle] = useState("Hello");
+
+//     return (
+//         <div className="counter">
+//             <h1>React Counter</h1>
+//             <p className="click_desc">
+
+//             Your count is <span>{count > 0 ? count : "dunno"}</span>.
+//       </p>
+
+//       <div className="button_container">
+
+//       <button onClick={() => setCount(count + 1)}>Add 1</button>
+//         <button onClick={() => setCount(count * 5)}>Multiply by 5</button>
+//         <button onClick={() => setCount(0)}>Reset</button>
+//       </div>
+
+//       </div>
+//   );
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+// const [score, setScore] = useState(); 
+
+//     
+
+//         const [score, setScore] = useState(0);
+//         const [title, setTitle] = useState("Hello");
+    
+//         return (
+//             <div className="counter">
+//                 <h1>React Counter</h1>
+//                 <p className="click_desc">
+    
+//                 Your count is <span>{count > 0 ? count : "dunno"}</span>.
+//           </p>
