8994e6fbdd3cfca7e61f94993d8577d39c1a223f
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-07-24 22:14:36 -0400

    buttons working

--- src/index.js
+++ src/index.js
@@ -7,42 +7,14 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-function App()  {
 
 
 
-    return (
 
 
-    )
-}
-
-
-// const [value, setValue] = useState();
-
-// function App() {
-
-//     const [count, setCount] = useState(0);
+//     
 //     const [title, setTitle] = useState("Hello");
 
-//     return (
-//         <div className="counter">
-//             <h1>React Counter</h1>
-//             <p className="click_desc">
-
-//             Your count is <span>{count > 0 ? count : "dunno"}</span>.
-//       </p>
-
-//       <div className="button_container">
-
-//       <button onClick={() => setCount(count + 1)}>Add 1</button>
-//         <button onClick={() => setCount(count * 5)}>Multiply by 5</button>
-//         <button onClick={() => setCount(0)}>Reset</button>
-//       </div>
-
-//       </div>
-//   );
-// }
 
 // const rootElement = document.getElementById("root");
 // ReactDOM.render(<App />, rootElement);
@@ -61,3 +33,6 @@ function App()  {
     
 //                 Your count is <span>{count > 0 ? count : "dunno"}</span>.
 //           </p>
+
+
+// <button onClick={() => setCount(0)}>Reset</button>
